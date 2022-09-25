191692e27afc94ba5a94c2ff8aeacd040dc4df61
Fabian Weik <fabian.weik@protonmail.com>
2022-09-25 18:10:26 +0200

    display song title and artist again

--- app/src/main/java/com/badradio/nz/metadata/MetadataReceiver.kt
+++ app/src/main/java/com/badradio/nz/metadata/MetadataReceiver.kt
@@ -2,17 +2,30 @@ package com.badradio.nz.metadata
 
 import com.badradio.nz.Config
 import com.badradio.nz.utilities.ListenersManager
-import com.badradio.nz.utilities.Log
-import okhttp3.OkHttpClient
+import okhttp3.*
+import java.io.IOException
 import java.util.*
 
-class MetadataReceiver : TimerTask() {
+class MetadataReceiver : TimerTask(), Callback {
     private val client =  OkHttpClient.Builder().build()
 
     override fun run() {
-        ListenersManager.onSongTitle("test", "bla")
+        val request = Request.Builder().url(Config.METADATA_URL).build()
+        client.newCall(request).enqueue(this)
+    }
 
+    override fun onFailure(call: Call, e: IOException) {
+        TODO("Think about what should happen on failure")
+    }
 
+    override fun onResponse(call: Call, response: Response) {
+        val stationStatus = stationStatusAdapter.fromJson(response.body()!!.source())
+            ?: throw IOException("Could not parse API response")
+
+        val songMetadata = SongMetadata.fromStationTrack(stationStatus.current_track)
+        ListenersManager.onSongTitle(songMetadata.title, songMetadata.artist)
+
+        // TODO: Fetch album art and call callback
     }
 
     companion object {
