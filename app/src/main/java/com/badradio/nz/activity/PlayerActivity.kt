75d8d4c5b1a0b370052ce4cb78928b07b8b054ed
Fabian Weik <fabian.weik@protonmail.com>
2022-10-07 15:57:51 +0200

    display metadata and start stream via activity

--- app/src/main/java/com/badradio/nz/activity/PlayerActivity.kt
+++ app/src/main/java/com/badradio/nz/activity/PlayerActivity.kt
@@ -5,97 +5,77 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.badradio.nz.R
 import android.content.Intent
-import android.media.AudioManager
+import android.graphics.BitmapFactory
 import com.badradio.nz.player.RadioManager
 import com.badradio.nz.player.PlaybackState
-import android.graphics.Bitmap
 import android.net.Uri
+import android.os.Handler
+import android.os.Looper
 import android.provider.Settings
-import android.view.View
-import android.widget.*
 import androidx.appcompat.app.AlertDialog
-import com.badradio.nz.Config
 import com.badradio.nz.databinding.ActivityPlayerBinding
+import com.badradio.nz.metadata.SongMetadata
 import com.badradio.nz.player.PlayerState
+import com.badradio.nz.player.RadioService
 import com.badradio.nz.utilities.PlayerStateObserver
 
 class PlayerActivity : AppCompatActivity(), PlayerStateObserver {
-
     private lateinit var binding: ActivityPlayerBinding
 
-    private var audioManager: AudioManager? = null
-    private var StationName: String? = null
-    private var StationDesc: String? = null
-    private var current = 0
-    private var current2 = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        RadioManager.bind(applicationContext)
+        tryRegisterAsPlayerStateObserver()
+
         binding = ActivityPlayerBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        onStateChange(
+            PlayerState(
+                PlaybackState.NOT_READY,
+                SongMetadata("24/7 Phonk Radio", "BADRADIO"),
+                BitmapFactory.decodeResource(resources, R.drawable.badradio)
+            )
+        )
 
         binding.imgAbout.setOnClickListener {
             val intent = Intent(this@PlayerActivity, AboutActivity::class.java)
             startActivity(intent)
         }
 
-        binding.imgBtnInfo.setOnClickListener { //sending to info page
+        binding.imgBtnInfo.setOnClickListener {
             val intent = Intent(this@PlayerActivity, InfoActivity::class.java)
             startActivity(intent)
         }
 
         binding.imgBtnPlay.setOnClickListener {
+            RadioManager.onPlay()
         }
-
 
         binding.imgBtnShare.setOnClickListener { shareApp(applicationContext) }
     }
 
-
-    private fun stopPlaying() {
-
-        // Updating button and song and artist textviews
-        binding.imgBtnPlay.setImageResource(R.drawable.btnplay)
-
-        binding.tvSongName.text = StationName
-        binding.tvArtist.text = StationDesc
-
-        // Changing status as stopped
-        // ListenersManager.onEvent(status)
-    }
-
-    /*
-    //checking if currently radio is playing
-    private val isPlaying: Boolean
-        private get() =//checking if currently radio is playing
-            null != RadioManager.service && RadioManager.service!!.isPlaying
-
-
-    fun updateButtons() {
-        val tvArtist = findViewById<View>(R.id.tv_Artist) as TextView
-        val tvSong = findViewById<View>(R.id.tv_songName) as TextView
-        if (isPlaying) {
-
-            //If another stream is playing, show this in the layout
-            if (RadioManager.service != null) {
-                binding.imgBtnPlay.setImageResource(R.drawable.btnplay)
-                tvSong.text = StationName
-                tvArtist.text = StationDesc
-            } else {
-                binding.imgBtnPlay.setImageResource(R.drawable.btnpause)
-                val sharedPreferences = getSharedPreferences("data", MODE_PRIVATE)
-                val song = sharedPreferences.getString("song", "")
-                val artist = sharedPreferences.getString("artist", "")
-                tvArtist.text = artist
-                tvSong.text = song
-            }
+    private fun tryRegisterAsPlayerStateObserver() {
+        if (RadioManager.ready) {
+            RadioManager.registerPlayerStateObserver(this)
         } else {
-            //If this stream is paused, adjust the buttons accordingly
-            binding.imgBtnPlay.setImageResource(R.drawable.btnplay)
-            tvSong.text = StationName
-            tvArtist.text = StationDesc
+            Handler(Looper.getMainLooper()).postDelayed({
+                tryRegisterAsPlayerStateObserver()
+            }, 100)
         }
     }
-     */
+
+    private val lastPlaybackState = PlaybackState.NOT_READY
+
+    override fun onStateChange(state: PlayerState) {
+        runOnUiThread {
+            binding.imgAlbumArt.setImageBitmap(state.art)
+
+            binding.textSongName.text = state.metadata.title
+            binding.textArtist.text = state.metadata.artist
+        }
+    }
 
     public override fun onStart() {
         super.onStart()
@@ -105,64 +85,6 @@ class PlayerActivity : AppCompatActivity(), PlayerStateObserver {
     public override fun onStop() {
         super.onStop()
         // ListenersManager.unregisterAsListener(this)
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        RadioManager.bind(applicationContext)
-    }
-
-    /*fun onEvent(status: PlayerState) {
-        when (status) {
-            PlayerState.LOADING -> {
-                binding.imgBtnPlay.setImageResource(R.drawable.btnpause)
-                binding.tvSongName.text = "Loading"
-            }
-            PlayerState.PAUSED   -> binding.imgBtnPlay.setImageResource(R.drawable.btnplay)
-            PlayerState.PLAYING  -> binding.imgBtnPlay.setImageResource(R.drawable.btnpause)
-            PlayerState.ERROR    -> Toast.makeText(
-                this,
-                R.string.no_stream,
-                Toast.LENGTH_SHORT
-            ).show()
-            else -> {
-                // dont do anythin? TODO: rethink
-            }
-        }
-    }*/
-
-/*
-    override fun onSongTitle(title: String, artist: String) {
-        runOnUiThread {
-            binding.tvSongName.text = title
-            binding.tvArtist.text = artist
-        }
-    }
-
-    override fun onAlbumArt(art: Bitmap) {
-        runOnUiThread {
-            binding.imgStationPlaying.setImageBitmap(art)
-        }
-    }
-
-    override fun onStateChange(state: PlaybackState) {
-        // TODO("Not yet implemented")
-    } */
-
-    private fun showSettingsDialog() {
-        val builder = AlertDialog.Builder(applicationContext)
-        builder.setTitle("Need Permissions")
-        builder.setMessage("This app needs permission to use this feature. You can grant them in app settings.")
-        builder.setPositiveButton("GOTO SETTINGS") { dialog, which ->
-            dialog.cancel()
-            openSettings()
-        }
-        builder.setNegativeButton("Cancel") { dialog, which -> dialog.cancel() }
-        builder.show()
     }
 
     private fun openSettings() {
@@ -183,16 +105,16 @@ class PlayerActivity : AppCompatActivity(), PlayerStateObserver {
         dialog.setIcon(R.mipmap.ic_launcher)
         dialog.setTitle(R.string.app_name)
         dialog.setMessage(resources.getString(R.string.message))
-        dialog.setPositiveButton(resources.getString(R.string.quit)) { dialogInterface, i ->
-            stopPlaying()
+        dialog.setPositiveButton(resources.getString(R.string.quit)) { _, _ ->
+            onPause()
             finish()
         }
-        dialog.setNegativeButton(resources.getString(R.string.minimize)) { dialogInterface, i -> minimizeApp() }
-        dialog.setNeutralButton(resources.getString(R.string.cancel)) { dialogInterface, i -> }
+        dialog.setNegativeButton(resources.getString(R.string.minimize)) { _, _ -> minimizeApp() }
+        dialog.setNeutralButton(resources.getString(R.string.cancel)) { _, _ -> }
         dialog.show()
     }
 
-    fun minimizeApp() {
+    private fun minimizeApp() {
         val intent = Intent(Intent.ACTION_MAIN)
         intent.addCategory(Intent.CATEGORY_HOME)
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
@@ -211,9 +133,5 @@ class PlayerActivity : AppCompatActivity(), PlayerStateObserver {
             sendIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
             context.startActivity(sendIntent)
         }
-    }
-
-    override fun onStateChange(state: PlayerState) {
-        // TODO("Not yet implemented")
     }
 }
--- app/src/main/java/com/badradio/nz/player/RadioManager.kt
+++ app/src/main/java/com/badradio/nz/player/RadioManager.kt
@@ -1,17 +1,17 @@
 package com.badradio.nz.player
 
 import android.app.Service.BIND_AUTO_CREATE
-import android.app.Service.STOP_FOREGROUND_REMOVE
 import android.os.IBinder
 import android.content.*
+import com.badradio.nz.utilities.PlayerStateObserver
 import com.badradio.nz.utilities.UserInputObserver
 
 object RadioManager : UserInputObserver {
     private lateinit var service: RadioService
-    private var serviceBound = false
+    var ready = false
 
     fun bind(context: Context) {
-        if (!serviceBound) {
+        if (!ready) {
             val intent = Intent(context, RadioService::class.java)
             context.bindService(intent, serviceConnection, BIND_AUTO_CREATE)
             context.startService(intent)
@@ -21,21 +21,18 @@ object RadioManager : UserInputObserver {
     private val serviceConnection: ServiceConnection = object : ServiceConnection {
         override fun onServiceConnected(componentName: ComponentName, binder: IBinder) {
             service = (binder as RadioService.RadioServiceBinder).service
-            serviceBound = true
+            ready = true
         }
 
         override fun onServiceDisconnected(componentName: ComponentName) {
-            serviceBound = false
+            ready = false
         }
     }
 
+    override fun onPlay() = service.onPlay()
+    override fun onPause() = service.onPause()
 
-    override fun onPlay() {
-        service.onPlay()
-    }
-
-    override fun onPause() {
-        service.onPause()
-    }
+    fun registerPlayerStateObserver(observer: PlayerStateObserver)
+        = service.registerPlayerStateObserver(observer)
 
 }
