2663906861afc2fb55bdc4031cc567dbafb08456
Clouds <me@cloudsftp.de>
2026-06-14 20:34:00 +0200

    update fallback radio.co url

--- app/src/main/java/nz/badradio/badradio/Config.kt
+++ app/src/main/java/nz/badradio/badradio/Config.kt
@@ -10,7 +10,7 @@ object Config {
     const val STATION_DATA_URL = "https://badradio.nz/streamingassets/stations.json"
     val FALLBACK_STATION_INFO = StationInfo(
         "Badradio",
-        "https://s2.radioco.net/$FALLBACK_STATION_ID/listen",
+        "https://s2.radio.co/$FALLBACK_STATION_ID/listen",
         "https://badradio.nz/streamingassets/RadioSchmadio/station-badradio.jpg",
         "Phonk, Trap, Hip Hop & Chill",
         "Badradio is officially back for 2020, with all your favourite artists. Visit badradio.nz"
